0f019fcfc28d65322711767f8b30f5687d6e82c7
shakar <shakarshahedan@gmail.com>
2022-01-16 16:14:47 +0300

    Added todoSearcjOptions

---
+++ src/TodoApi/Models/QueryOptions/TodoSearchOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models.QueryOptions
+{
+  public class TodoSearchOptions
+  {
+    public DateTime? Date { get; set; }
+  }
+}
