987a54d5fa76036aa65f0500a22d38aa15464245
shakar <shakarshahedan@gmail.com>
2022-01-25 10:40:30 +0300

    Made program.cs more readable

--- src/TodoApi/Extensions/ConfigureAuth.cs
+++ src/TodoApi/Extensions/ConfigureAuth.cs
@@ -7,9 +7,18 @@ public static partial class StartupConfigurations
 
   public static IServiceCollection ConfigureAuth(this IServiceCollection services, IConfiguration config)
   {
+    services.AddAuthentication(options =>
+    {
+      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+      options.Authority = config["Authentication:Domain"];
+      options.Audience = config["Authentication:Audience"];
+    });
 
-    return services.AddAuthorization(options => options.AddPolicy("read:weather", policy => policy.Requirements.Add(new HasScopeRequirement("read:weather", $"https://{config["Authentication:Domain"]}/"))));
-
+    services.AddAuthorization(options => options.AddPolicy("read:weather", policy => policy.Requirements.Add(new HasScopeRequirement("read:weather", $"https://{config["Authentication:Domain"]}/"))));
+    return services;
   }
 
 }
